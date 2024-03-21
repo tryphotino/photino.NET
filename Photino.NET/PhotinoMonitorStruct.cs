32a754d9b9521e566addeeecb59ecbbcb43b0aa8
Mike Yeager <MYeager@eps-software.com>
2024-03-21 10:04:45 -0600

    Updated namespace.

--- Photino.NET/PhotinoMonitorStruct.cs
+++ Photino.NET/PhotinoMonitorStruct.cs
@@ -1,7 +1,7 @@
 ﻿using System.Drawing;
 using System.Runtime.InteropServices;
 
-namespace PhotinoNET
+namespace Photino.NET
 {
     /// <summary>
     /// Represents a 2D rectangle in a native (integer-based) coordinate system.
